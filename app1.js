0d5fe68a8b03b51bf4ebdce08a05a058cd1432d1
tanc41 <33790473+tanc41@users.noreply.github.com>
2023-10-24 05:27:38 +0300

    Update app1.js

--- app1.js
+++ app1.js
@@ -5,19 +5,10 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#FF00FF";
 
-let btn = document.getElementById("btn");
-
-btn.addEventListener("click", function(){
-    tg.MainButton.setText("Сообщение отправлено!");
-    tg.MainButton.show();
-    tg.sendData("Привет Андрей");
-    tg.answerWebAppQuery("HELLO")
-});
-
 
         const orderForm = document.getElementById("orderForm");
 
-        orderForm.addEventListener("submit", function () {
+        orderForm.addEventListener("click", function () {
             // Получение данных из формы
             const category = document.getElementById("category").value;
             const service = document.getElementById("service").value;

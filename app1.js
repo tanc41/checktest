9dc79e767e4519e9988eb77945654f247f2af6e9
tanc41 <33790473+tanc41@users.noreply.github.com>
2023-10-25 01:43:07 +0300

    Update app1.js

--- app1.js
+++ app1.js
@@ -13,3 +13,25 @@ btn.addEventListener("click", function(){
     tg.sendData("Привет Андрей");
     tg.answerWebAppQuery("HELLO")
 });
+
+    document.getElementById('order-form').addEventListener('submit', function (event) {
+      event.preventDefault(); 
+
+      var category = document.getElementById('category').value;
+      var service = document.getElementById('service').value;
+      var type = document.getElementById('type').value;
+      var link = document.getElementById('link').value;
+      var quantity = document.getElementById('quantity').value;
+        
+      var formData = {
+        category: category,
+        service: service,
+        type: type,
+        link: link,
+        quantity: quantity
+      };
+            tg.MainButton.setText("Сообщение отправлено!");
+    tg.MainButton.show();
+    tg.sendData(formData);
+
+    });

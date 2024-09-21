b0a2a98d041ae50148a3f1e223248ba60498b27f
tanc41 <33790473+tanc41@users.noreply.github.com>
2024-09-21 05:38:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ btn.addEventListener("click", function(){
     tg.MainButton.setText("Сообщение отправлено!");
     tg.MainButton.show();
     tg.sendData("Привет Андрей");
-    tg.answerWebAppQuery("HELLO")
+    tg.answerWebAppQuery("HELLO");
 });
